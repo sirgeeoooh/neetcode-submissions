e5cd5b39d97e980ea4ba11a35c2842a981b6ea6b
sirgeeoooh <sergio.b.herrera9415@gmail.com>
2026-07-13 21:55:24 -0700

    Add: number-of-students-unable-to-eat-lunch - submission-0

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-0.py
@@ -0,0 +1,48 @@
+class DDLLNode:
+    def __init__(self, val, prev = None, nxt = None):
+        self.val = val
+        self.prev = prev
+        self.next = nxt
+
+    def dequeue(self):
+        self.prev.next = self.next
+        self.next.prev = self.prev
+
+    def queue(self, end):
+        self.prev = end.prev
+        self.prev.next = self
+        self.next = end
+        end.prev = self
+
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        front = DDLLNode(0)
+        end = DDLLNode(0)
+        front.next = end
+        end.prev = front
+        res = len(students)
+
+        for _ in students:
+            node = DDLLNode(_)
+            node.queue(end)
+        
+        count = 0
+        
+
+        while sandwiches and count < res:  
+            cur_node = front.next
+            if cur_node.val == sandwiches[0]:
+                cur_node.dequeue()
+                res -= 1
+                sandwiches.pop(0)
+                count = 0
+            else:
+                cur_node.next.prev = front
+                front.next = cur_node.next
+                cur_node.queue(end)
+                count += 1
+
+        return res
+
+                    
